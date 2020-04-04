0b5f086453278cdc9ac0d20e8d62dfb508cc30c2
Igor <goga.danilov10@gmail.com>
2020-04-04 20:23:08 +0400

    Add files via upload

---
+++ gentoo_install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+(
+  echo g;
+  echo n;
+  echo 1;
+  echo;
+  echo +2m;
+  echo t;
+  echo 4;
+  echo n;
+  echo 2;
+  echo;
+  echo +128m;
+  echo n;
+  echo 3;
+  echo;
+  echo;
+) | fdisk /dev/sda
+
+mkfs.vfat /dev/sda2
+mkfs.ext4 /dev/sda3
+mount /dev/sda3 /mnt/gentoo
