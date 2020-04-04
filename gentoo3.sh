bf861562c207a719b3f8d34688b1234e3c9c4e44
Igor <goga.danilov10@gmail.com>
2020-04-05 00:16:23 +0400

    Update gentoo3.sh

--- gentoo3.sh
+++ gentoo3.sh
@@ -1,7 +1,7 @@
 !#/bin/bash
 
 mount --types proc /proc /mnt/gentoo/proc
-root #mount --rbind /sys /mnt/gentoo/sys
-root #mount --make-rslave /mnt/gentoo/sys
-root #mount --rbind /dev /mnt/gentoo/dev
-root #mount --make-rslave /mnt/gentoo/dev 
+mount --rbind /sys /mnt/gentoo/sys
+mount --make-rslave /mnt/gentoo/sys
+mount --rbind /dev /mnt/gentoo/dev
+mount --make-rslave /mnt/gentoo/dev 
