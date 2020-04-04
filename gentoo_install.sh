990c1a538e6f5692941cbbfb6260e82988dd8e3d
Igor <goga.danilov10@gmail.com>
2020-04-04 23:01:44 +0400

    Update gentoo_install.sh

--- gentoo_install.sh
+++ gentoo_install.sh
@@ -26,4 +26,4 @@ ntpd -q -g
 cd /mnt/gentoo
 wget https://mirror.yandex.ru/gentoo-distfiles/releases/amd64/autobuilds/current-stage3-amd64/stage3-amd64-20200401T214502Z.tar.xz
 
-tar xpvf stage3-amd64-20200401T214502Z.tar.xz --xattrs-include='*.*' --numeric-owner
+tar xpf stage3-amd64-20200401T214502Z.tar.xz --xattrs-include='*.*' --numeric-owner
