240dd32beaaf02aaafa72550e3ffa7879d9b88da
Igor <goga.danilov10@gmail.com>
2020-07-10 20:55:11 +0400

    Update gentoo_install.sh

--- gentoo_install.sh
+++ gentoo_install.sh
@@ -27,3 +27,10 @@ cd /mnt/gentoo
 wget https://mirror.yandex.ru/gentoo-distfiles/releases/amd64/autobuilds/current-stage3-amd64/stage3-amd64-20200401T214502Z.tar.xz
 
 tar xpf stage3-amd64-20200401T214502Z.tar.xz --xattrs-include='*.*' --numeric-owner
+
+
+mount --types proc /proc /mnt/gentoo/proc
+mount --rbind /sys /mnt/gentoo/sys
+mount --make-rslave /mnt/gentoo/sys
+mount --rbind /dev /mnt/gentoo/dev
+mount --make-rslave /mnt/gentoo/dev 
