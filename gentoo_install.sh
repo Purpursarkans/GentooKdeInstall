143c9f5a22bb0fc0261bdda1013441855facd032
Igor <goga.danilov10@gmail.com>
2020-07-11 08:55:22 +0400

    Update gentoo_install.sh

--- gentoo_install.sh
+++ gentoo_install.sh
@@ -76,4 +76,10 @@ echo -e "WARNING!!!\ninstall sleep for 30 sec, check list"
 sleep 30
 
 env-update && source /etc/profile && export PS1="(chroot) ${PS1}"
+loadkeys ru
+setfont cyr-sun16
 
+
+emerge --ask sys-kernel/gentoo-sources
+
+echo -e "/dev/sda2	/boot	vfat	defaults	0 2" >> /etc/fstab
